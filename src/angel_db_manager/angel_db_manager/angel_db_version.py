1a580f3ed34733c321815b72c0e80d15c4f53a25
Minsoo Song <sms0432@gmail.com>
2022-03-10 07:30:19 +0900

    edit version node & add activate_node  script

--- src/angel_db_manager/angel_db_manager/angel_db_version.py
+++ src/angel_db_manager/angel_db_manager/angel_db_version.py
@@ -3,7 +3,10 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Bool
 from std_msgs.msg import String
-
+import pymongo
+MONGO_HOSTNAME = '192.168.0.243'
+MONGO_PORT = '27017'
+MONGO_DB = 'wasp'
 
 class MinimalPublisher(Node):
 
@@ -11,12 +14,16 @@ class MinimalPublisher(Node):
         super().__init__('db_version')
         self.publisher_liveness = self.create_publisher(String, '/dbmanger_version', 10)
         timer_period2 = 2  # seconds
+        self.client = pymongo.MongoClient('mongodb://'+MONGO_HOSTNAME+':'+MONGO_PORT)
         self.timer_liveness = self.create_timer(timer_period2, self.timer_callback_liveness)
         self.i = 0
 
     def timer_callback_liveness(self):
         msg = String()
-        msg.data = "0.1.0"
+        msg.data = "0.1"
+        db = self.client.wasp.corever
+        version_db = {"id" : "M30", "version" : msg.data}
+        db.insert_one(version_db)
         self.publisher_liveness.publish(msg)
         self.get_logger().info('version: "%s"' % msg.data)
 
